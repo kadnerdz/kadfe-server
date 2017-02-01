94b75d406e38e6000853ef5372f44df52bcd8483
ashwinv11 <ashwinv11@gmail.com>
2017-01-31 19:33:46 -0800

    moving to github cause gitlab is dead :( also finalizing db saving

---
+++ README.md
@@ -0,0 +1,21 @@
+# Backend Code For Kadfe!
+
+## Endpoints
+---
+
+### /brewed
+
+*"Coffee is made"*
+POST request for when coffee has been made.
+
+
+
+### /claimed
+*"User claims coffee"*
+PUT request for when coffee has been claimed.
+
+
+
+### /status
+*"???"*
+GET request to check the coffee status.
---
+++ app.js
@@ -0,0 +1,26 @@
+var JsonDB = require('node-json-db');
+var db = new JsonDB("db", true, false);
+var express = require('express');
+var app = express();
+
+app.post('/brewed', function (req, res) {
+  db.push("/state1","true");
+  db.reload();
+  res.send('Coffee is made!');
+});
+
+app.put('/claimed', function (req, res) {
+  if(db.getData("/state1") == "true"){
+    db.push("/state1","false");
+    db.reload();
+  }
+  res.send(db.getData("/state1"));
+});
+
+app.get('/status', function (req, res) {
+  res.send(db.getData("/state1"));
+});
+
+app.listen(3000, function () {
+  console.log('Welcome to the Kadfe Backend!');
+});
